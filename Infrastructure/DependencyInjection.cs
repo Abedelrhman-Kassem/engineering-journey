87d7a806866f107350c6ea4a1a9f6d6a16862323
Abedelrhman-Kassem <107558049+Abedelrhman-Kassem@users.noreply.github.com>
2026-08-29 10:58:08 +0300

    docs: correct configuration paragraph to match the code. (#13)
    * feat: add DI in Infra and call in Host
    
    * fix: Make IEmailSender Signleton it don't has any state, make it async
    
    * chore: Add DI in Application, pass configuration to application, infra
    
    * fix: Remove Public from interface IemailSender, add await in implemention
    
    * fix: Delete Application DI and move package to infra
    
    * fix: fix some Issues in IEmailSender, EmailSender
    
    - Add CancellationToken to them
    - Add Sealed to EmailSender
    - Remove Async Await to remove compiler work with no need
    
    * docs: Add Compistion root section in Architecture md file
    
    * fix: fix IEmailSender
    
    - add recipient to IEmailSender
    - Remove Unused using Application in Program file
    - Remove Configuration parameter from DI Infra
    
    * fix: Remove Configuration using and package
    
    * docs: correct configuration paragraph to match the code

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using Application.Interfaces;
 using Infrastructure.Services;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
