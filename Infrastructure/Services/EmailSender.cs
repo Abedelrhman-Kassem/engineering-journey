b9d45e7e385dfda097e362978f62c7a6aee0793f
Abedelrhman-Kassem <abedelrhmankassem@gmail.com>
2026-09-19 18:45:11 +0300

    fix: Remove 3 seconds Delay in EmailService

--- Infrastructure/Services/EmailSender.cs
+++ Infrastructure/Services/EmailSender.cs
@@ -6,7 +6,6 @@ internal sealed class EmailSender : IEmailSender
 {
     public async Task SendEmailAsync(string email, string subject, string body, CancellationToken cancellationToken)
     {
-        await Task.Delay(3000, cancellationToken);
         Console.WriteLine($"Sending email to {email} with subject: {subject} and body: {body}");
     }
 }
